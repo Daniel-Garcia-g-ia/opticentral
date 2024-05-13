abb77c9810d929d387a65d996bd2efc483d5e9b7
Daniel Garcia <daniel.g.1393@gmail.com>
2024-05-13 09:22:48 -0500

    [add]start logged

--- opticentral/src/components/home/index.jsx
+++ opticentral/src/components/home/index.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import Navbar from "../navbar"
 import { AuthContext } from "../context/AuthContext";
 import { Navigate } from "react-router-dom";
 
@@ -6,7 +7,8 @@ function Home() {
 
     return (
         <>
-            <div>Welcome to Home</div>
+        <Navbar />
+            
         </>
 
     )
