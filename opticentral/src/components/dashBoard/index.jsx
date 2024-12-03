5ff7917cbbee4e1210cbcc87524a34ad8f37bb12
Daniel Garcia <daniel.g.1393@gmail.com>
2024-12-03 18:53:08 -0500

    [bug]bug to commets line code

--- opticentral/src/components/dashBoard/index.jsx
+++ opticentral/src/components/dashBoard/index.jsx
@@ -72,11 +72,11 @@ function DashBoard() {
 
         if (permissonsRole) {
             setActiveTime(false)
-            /* setActivateDetailsProduction(false) */
+            setActivateDetailsProduction(false)
             return
         } else {
             setActiveTime(true)
-            /* setActivateDetailsProduction(true) */
+            setActivateDetailsProduction(true)
             setSelectedDate(!selectedDate)
 
         }
